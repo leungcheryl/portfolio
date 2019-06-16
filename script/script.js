f32a46265b65a704c3f72339b78188d062497f9f
leungcheryl <leung.cheryl@icloud.com>
2019-06-16 05:38:02 -0400

    styled main page

--- script/script.js
+++ script/script.js
@@ -2,13 +2,36 @@ const lineDrawing = anime({
     targets: '.main-title .lines path',
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: 'easeInOutSine',
-    duration: 5000,
+    duration: 9000,
     delay: function(el, i) { return i * 250 },
     direction: 'alternate',
     loop: true
   });
 
+  const typed = new Typed('.typed', {
+    stringsElement: '.typed-strings'
+  });
 
-  $(document).ready(function(){
-	console.log('ready')
+$(document).ready(function(){
+
+    function toggleNav() {
+      $('.nav-icon').toggleClass('open');
+
+      if ($('.nav-icon').hasClass('open')) {
+        setTimeout(function(){
+          $('.nav-content').toggleClass('visible-content')
+        },400);
+      } else {
+        $('.nav-content').toggleClass('visible-content')
+      }
+      $('.nav-overlay').toggleClass('visible');
+      $('.visible').css('width', '100%');
+    }
+
+    $('.nav-icon').click(function(){
+      toggleNav();
+    });
+
+
+    
 });
